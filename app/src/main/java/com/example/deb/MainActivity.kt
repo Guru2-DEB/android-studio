728b43d811186e46b90402c4951ab5a8715d907c
Nugungu <cho563015@naver.com>
2025-07-24 12:09:28 +0900

    BottomNavigationBar_Connect

--- app/src/main/java/com/example/deb/MainActivity.kt
+++ app/src/main/java/com/example/deb/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.deb
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val studyNewListFragment = StudyNewListFragment()
+    private val historyFragment = HistoryFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
+        bottomNavBar.setOnItemSelectedListener  {
+            val selectedFragment: Fragment = when (it.itemId){
+                R.id.Home -> homeFragment
+                R.id.Study -> studyNewListFragment
+                R.id.History -> historyFragment
+                else -> homeFragment
+            }
+
+            supportFragmentManager.beginTransaction().replace(R.id.mainContainer, selectedFragment).commit()
+            true
         }
+
     }
 }
